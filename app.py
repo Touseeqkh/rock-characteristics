3866f17077fe85e23d9181a46ba5be987bf13191
Touseeqkh <163164883+Touseeqkh@users.noreply.github.com>
2025-12-15 16:04:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import sklearn
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
 
 # ==================================================
 # Page Configuration
